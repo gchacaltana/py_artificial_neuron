bfebbfc9eabf89b8767ec6fe58af38f73452893f
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-12-11 22:30:53 -0500

    insert transfer function

--- App.py
+++ App.py
@@ -12,12 +12,15 @@ class App(object):
         self.w = []  # pesos de las entradas
         self.u = 1  # umbral
         self.u_w = 0.5  # peso de umbral
+        self.transfer = 0
 
     def insert_values(self):
-        self.inputs_x()
-        self.inputs_w()
+        self.insert_x()
+        self.insert_w()
+        self.insert_umbral()
+        self.insert_activation()
 
-    def inputs_x(self):
+    def insert_x(self):
         self.entries = int(
             input("Ingrese cantidad de entradas (x) de la neurona artificial: "))
         c = 0
@@ -25,18 +28,24 @@ class App(object):
             c += 1
             self.x.append(float(input("Ingrese el valor de X%s: " % c)))
 
-    def inputs_w(self):
+    def insert_w(self):
         if (int(self.entries) > 0):
             c = 0
             while len(self.w) < self.entries:
                 c += 1
                 self.w.append(float(input("Ingrese el valor de W%s: " % c)))
 
-    def inputs_umbral(self):
+    def insert_umbral(self):
         if (int(self.entries) > 0):
             self.u = float(input("Ingrese el valor del umbral: "))
             self.u_w = float(input("Ingrese el peso del umbral: "))
 
+    def insert_activation(self):
+        print("Funcion de Transferencia")
+        print("[1] Sigmoidal")
+        print("[2] Escalon Unitario")
+        while int(self.transfer)!=0:
+            self.transfer = int(input("Ingrese una opcion: "))
 
 if __name__ == "__main__":
     application = App()
