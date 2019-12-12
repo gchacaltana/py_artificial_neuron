4bd7ca573b2b68ce9fe30c833338190001a6e3da
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-12-11 22:22:28 -0500

    first commit Artificial Neuron Structure

---
+++ ArtificialNeuron.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Artificial Neuron
+__author__ = "Gonzalo Chacaltana Buleje"
+__email__ = "gchacaltanab@gmail.com"
+
+class ArtificialNeuron(object):
+    def __init__(self, x, w, y):
+        self.x = x
+        self.w = w
+        self.y = y
